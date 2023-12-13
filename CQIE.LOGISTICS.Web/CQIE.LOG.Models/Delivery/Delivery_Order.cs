863c5b089b409e9144647d1ebcce9faf8573f90e
ausnatki <109503108+ausnatki@users.noreply.github.com>
2023-12-13 18:11:21 +0800

    312

--- CQIE.LOGISTICS.Web/CQIE.LOG.Models/Delivery/Delivery_Order.cs
+++ CQIE.LOGISTICS.Web/CQIE.LOG.Models/Delivery/Delivery_Order.cs
@@ -12,7 +12,7 @@ namespace CQIE.LOG.Models.Delivery
     public class Delivery_Order
     {
         public int Id { get; set; }
-        public int Car_Id { get; set; }
+        public int Car_Id { get; set; }  
         public int WayBill_Id{get;set;}
         public int Delivery_man_Id { get; set; }
         public DateTime Delivery_data { get; set; }        
@@ -20,6 +20,6 @@ namespace CQIE.LOG.Models.Delivery
         public Delivery_Man Delivery_Man { get; set; }
         public Waybill.WayBill WayBill { get; set; }
         public Expenses.Expenses Expenses { get; set; }
-
+           
     }
 }
